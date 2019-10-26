3418070e35202263d48bdbbc093a4e52aada9d6f
Wesley Ferreira <wesleyferreirasb@gmail.com>
2019-10-26 17:53:51 -0300

    Inserindo cores nas pecas impressas na classe Tela

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -10,15 +10,14 @@ namespace xadrez_console
         {
             try
             {
-                /* Tabuleiro tab = new Tabuleiro(8, 8);
+                 Tabuleiro tab = new Tabuleiro(8, 8);
                  tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                  tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                  tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-                 Tela.ImprimirTabuleiro(tab);*/
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 7));
 
-                PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-                Console.WriteLine(pos.ToString());
-                Console.WriteLine(pos.ToPosicao());
+                Tela.ImprimirTabuleiro(tab);
             }
             catch(TabuleiroException e)
             {
