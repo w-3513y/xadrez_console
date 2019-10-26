b6ef6bf992f4bf4e9e8157e184199c31acb93c2c
Wesley Ferreira <wesleyferreirasb@gmail.com>
2019-10-26 18:07:16 -0300

    Inclusao da classe PartidaDeXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -10,14 +10,9 @@ namespace xadrez_console
         {
             try
             {
-                 Tabuleiro tab = new Tabuleiro(8, 8);
-                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                 tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                 tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
-                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 7));
+                PartidaDeXadrez partida = new PartidaDeXadrez();                 
 
-                Tela.ImprimirTabuleiro(tab);
+                Tela.ImprimirTabuleiro(partida.Tab);
             }
             catch(TabuleiroException e)
             {
