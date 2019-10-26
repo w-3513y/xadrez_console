9fd173a1d2e089b61840a502aeeefc8cecdb8b09
Wesley Ferreira <wesleyferreirasb@gmail.com>
2019-10-26 17:34:47 -0300

    Adicao de excessao no tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,4 +1,5 @@
 ﻿namespace tabuleiro
+
 {
     class Tabuleiro
     {
@@ -18,10 +19,39 @@
             return Pecas[linha, coluna];
         }
 
+        public Peca Peca(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
         public void ColocarPeca(Peca p, Posicao posicao)
         {
             Pecas[posicao.Linha, posicao.Coluna] = p;
             p.Posicao = posicao;
+        }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha <0 || pos.Linha >= Linhas || pos.Coluna <0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posicao inválida!");
+            }
+            
         }
     }
 }
